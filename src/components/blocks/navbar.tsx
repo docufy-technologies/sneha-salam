b11dbf74f84935612fee70b2e56c50e214d4ca6e
Rakibul Hasan Ratul <rakibulhasanratul@proton.me>
2026-05-15 16:48:04 +0600

    feat: add calendly link to nav bar

--- src/components/blocks/navbar.tsx
+++ src/components/blocks/navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Link, type LinkProps } from "@tanstack/react-router";
+import { Link } from "@tanstack/react-router";
 import { motion } from "framer-motion";
 import type React from "react";
 import { useRef, useState } from "react";
@@ -25,14 +25,20 @@ function NavBar() {
       className="mx-auto flex w-fit text-nowrap rounded-full border-2 border-secondary bg-transparent backdrop-blur-xl z-100 fixed top-1 left-1/2 -translate-x-1/2 px-2 py-1 max-sm:p-1 gap-4 max-sm:gap-0"
       onMouseLeave={() => setPosition((pv) => ({ ...pv, opacity: 0 }))}
     >
-      <NavTab setPosition={setPosition} to="/">
-        Home
+      <NavTab setPosition={setPosition}>
+        <Link to="/">Home</Link>
       </NavTab>
-      <NavTab setPosition={setPosition} to="/about">
-        About
+      <NavTab setPosition={setPosition}>
+        <Link to="/about">About</Link>
       </NavTab>
-      <NavTab setPosition={setPosition} to="/">
-        Contact Me
+      <NavTab setPosition={setPosition}>
+        <a
+          href="https://calendly.com/snehasalamhere/30min"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Book A Meeting
+        </a>
       </NavTab>
 
       <Cursor position={position} />
@@ -44,36 +50,32 @@ const NavTab = ({
   children,
   setPosition,
   className,
-  to,
 }: {
   children: React.ReactNode;
   setPosition: (position: Position) => void;
   className?: ClassNameValue;
-  to: LinkProps["to"];
 }) => {
   const ref = useRef<HTMLLIElement>(null);
   return (
-    <Link to={to}>
-      <li
-        ref={ref}
-        onMouseEnter={() => {
-          if (!ref.current) return;
+    <li
+      ref={ref}
+      onMouseEnter={() => {
+        if (!ref.current) return;
 
-          const { width } = ref.current.getBoundingClientRect();
-          setPosition({
-            width,
-            opacity: 1,
-            left: ref.current.offsetLeft,
-          });
-        }}
-        className={cn(
-          "relative z-10 block cursor-pointer px-3 py-1.5 rounded-full hover:bg-secondary text-sm!",
-          className,
-        )}
-      >
-        {children}
-      </li>
-    </Link>
+        const { width } = ref.current.getBoundingClientRect();
+        setPosition({
+          width,
+          opacity: 1,
+          left: ref.current.offsetLeft,
+        });
+      }}
+      className={cn(
+        "relative z-10 block cursor-pointer px-3 py-1.5 rounded-full hover:bg-secondary text-sm!",
+        className,
+      )}
+    >
+      {children}
+    </li>
   );
 };
 
